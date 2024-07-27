95429d41f08a1c0b0867c4907b43e41520a25412
Samuel Akinbile <samakinbile@gmail.com>
2024-07-27 06:41:48 +0000

    template

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin 
 
+
+admin.site.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    
+    summermote_fields = ('content')
 # Register your models here.
