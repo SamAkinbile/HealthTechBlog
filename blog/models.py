8181ca5e53302e8632945136982911f0f57a4be4
Samuel Akinbile <samakinbile@gmail.com>
2024-08-02 13:39:36 +0000

    models.py

--- blog/models.py
+++ blog/models.py
@@ -5,27 +5,28 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-
 class Post(models.Model):
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
-    class Meta:
+    
+ class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
-        return self.title
+        return f"{self.title} | written by {self.author}"
 
     def number_of_likes(self):
         return self.likes.count()
@@ -47,6 +48,6 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.body} by {self.author}"
 
     
