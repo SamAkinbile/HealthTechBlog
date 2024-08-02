0e36c649275048324399ed137b7099e82c1dfd6c
Samuel Akinbile <samakinbile@gmail.com>
2024-08-02 13:27:30 +0000

    models.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Post)
+@admin.site.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
@@ -13,7 +13,7 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 
-@admin.register(Comment)
+@admin.site.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
@@ -22,3 +22,4 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
+
--- blog/models.py
+++ blog/models.py
@@ -40,8 +40,13 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
-    class Meta:
+   class Comment(models.Model):
+    # …
+   
+   class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+    
