4b8e0e71302a22871899df67c7cdfc3b3686d3cf
Samuel Akinbile <samakinbile@gmail.com>
2024-08-03 15:30:40 +0000

    view.py

--- blog/views.py
+++ blog/views.py
@@ -60,6 +60,18 @@ class PostDetail(View):
                 "comments": comments,
                 "commented": True,
                 "comment_form": comment_form,
-                "liked": liked
+                "liked": liked,
+                "comment_form": CommentForm()
+
             },
         )
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))       
