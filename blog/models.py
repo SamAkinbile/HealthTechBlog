82e43feadc1a6c8a98ce7951181a01387f6dce77
Samuel Akinbile <samakinbile@gmail.com>
2024-08-03 11:23:43 +0000

    models.py done

--- blog/models.py
+++ blog/models.py
@@ -6,20 +6,16 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
-    """
-    Stores a single blog post entry related to :model:`auth.User`.
-    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
 
 class Meta:
